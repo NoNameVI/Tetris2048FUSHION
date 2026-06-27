2df1d6fcf7bedf9e16677599d483992b9a1c0cb8
Ivanovic <111422951+NoNameVI@users.noreply.github.com>
2026-06-27 18:13:32 +0700

    Implement explosion trigger method in QuantumCascadeMode
    Added method to trigger explosion effect in QuantumCascadeMode.

--- src/tetris2048fushion/modes/QuantumCascadeMode.java
+++ src/tetris2048fushion/modes/QuantumCascadeMode.java
@@ -10,6 +10,18 @@ package tetris2048fushion.modes;
  * @author Hoàng
  */
 public abstract class QuantumCascadeMode implements IGameMode {
-    // TODO (Hoàng): Kích hoạt phản ứng nổ năng lượng bán kính 3x3 khi hợp nhất đạt khối 2048 
-    // TODO (Hoàng): Xử lý chuỗi combo rơi tự động (Cascade) dọn sạch bàn chơi 
+    
+    // Kích hoạt phản ứng nổ năng lượng theo bán kính chỉ định
+    protected void triggerExplosion(int[][] matrix, int centerRow, int centerCol, int radius, int maxRow, int maxCol) {
+        int startRow = Math.max(0, centerRow - radius);
+        int endRow = Math.min(maxRow - 1, centerRow + radius);
+        int startCol = Math.max(0, centerCol - radius);
+        int endCol = Math.min(maxCol - 1, centerCol + radius);
+
+        for (int i = startRow; i <= endRow; i++) {
+            for (int j = startCol; j <= endCol; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
 }
